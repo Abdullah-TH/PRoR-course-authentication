2c2987132c0bcd4ca797ed0b8fc463722f9edfb1
Abdullah Althobetey <abdullah.th@me.com>
2023-10-28 20:41:45 +0300

    Add tests

--- test/controllers/registration_controller_test.rb
+++ test/controllers/registration_controller_test.rb
@@ -2,12 +2,13 @@ require "test_helper"
 
 class RegistrationControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get registration_new_url
+    get new_registration_url
     assert_response :success
   end
 
   test "should get create" do
-    get registration_create_url
-    assert_response :success
+    post registration_index_url, params: { user: { email: "a@a.com", password: "123" } }
+    # assert_response :success
+    assert_equal "user created", flash[:notice]
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "email must be present" do
+    user = User.new(email: "", password: "123")
+    assert user.invalid?
+  end
+
+  test "password must be present" do
+    user = User.new(email: "a@a.com", password: "")
+    assert user.invalid?
+  end
+
+  test "user with email and password is valid" do
+    user = User.new(email: "a@a.com", password: "123")
+    assert user.valid?
+  end
+
 end
