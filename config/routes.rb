7f1d12ae661983abcfdb720949e23dcda1281c7d
Abdullah Althobetey <abdullah.th@me.com>
2023-10-29 01:30:50 +0300

    Generate Post scaffold and add association between user and posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "dashboard/index"
   resources :login, only: [:new, :create, :destroy]
   resources :registration, only: [:new, :create]
+  resources :posts
 end
