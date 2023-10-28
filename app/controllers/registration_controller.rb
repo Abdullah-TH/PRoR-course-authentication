42dbd4adbbfc7ca5e40b3456f5a7a2012674762b
Abdullah Althobetey <abdullah.th@me.com>
2023-10-28 17:35:18 +0300

    Implement signup

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,22 @@
+class RegistrationController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "user created"
+      render :new, status: :unprocessable_entity
+    else
+      flash[:notice] = "something went wrong"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
