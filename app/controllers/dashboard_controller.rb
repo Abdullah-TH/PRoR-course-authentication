f6a6934679213a0e62045cadea6d211c5c4fb760
Abdullah Althobetey <abdullah.th@me.com>
2023-10-30 00:44:10 +0300

    add navigation for dashboard and posts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def index
     unless session[:user_id]
+      flash[:notice] = "You have to log in first"
       redirect_to new_login_path
     end
   end
